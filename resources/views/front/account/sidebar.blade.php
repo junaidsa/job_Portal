58eaa981d3caaefd3c2da754506552ea28bb6939
junaidsa <94611843+junaidsa@users.noreply.github.com>
2024-04-18 03:05:00 -0700

    pen feature is done

--- resources/views/front/account/sidebar.blade.php
+++ resources/views/front/account/sidebar.blade.php
@@ -1,11 +1,15 @@
 <div class="card border-0 shadow mb-4 p-3">
-    <div class="s-body text-center mt-3">
-        <img src="{{ asset('profile_image/thum/' . (Auth::user()->image ?? 'avatar.png')) }}" alt="avatar" class="rounded-circle img-fluid" style="width: 150px;">
-
+    <div class="s-body text-center mt-3 position-relative">
+        <div class="position-absolute" style="left: 50%; z-index:999;  top:-10%; box-shadow: -13px 20px 20px 16px #f4f4f4; border-radius: 20px;">
+            <svg data-bs-target="#exampleModal" data-bs-toggle="modal" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pen" viewBox="0 0 16 16">
+                <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001m-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708z"/>
+              </svg>
+        </div>
+        <img src="{{ asset('profile_image/thum/' . (Auth::user()->image ?? 'avatar.jpg')) }}" alt="profile_image" class="rounded-circle img-fluid" style="width: 150px;">
         <h5 class="mt-3 pb-0">{{Auth::user()->name}}</h5>
         <p class="text-muted mb-1 fs-6">{{Auth::user()->designation}}</p>
         <div class="d-flex justify-content-center mb-2">
-            <button data-bs-toggle="modal" data-bs-target="#exampleModal" type="button" class="btn btn-primary">Change Profile Picture</button>
+            {{-- <button data-bs-toggle="modal" data-bs-target="#exampleModal" type="button" class="btn btn-primary">Change Profile Picture</button> --}}
         </div>
     </div>
 </div>
